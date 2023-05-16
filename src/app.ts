62ab72ca2be276af38870d96f1505a3ba5325cd3
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-05-16 09:30:21 +0600

    inserting data to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { Schema, model } from "mongoose";
 
 const app: Application = express();
 // using cors
@@ -14,12 +15,81 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
     step1-interface
     step 2- schema
     step3-model
-    step4-database query
+    step4-database query on model
 
     */
-//    creating interface
+  // 1. Create an interface representing a document in MongoDB.
+  interface IUser {
+    id: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "female";
+    role: "Student";
+    email?: string;
+    password: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanentAddress: String;
+  }
+  // // 2. Create a Schema corresponding to the document interface.
+  const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: { type: String },
+    role: { type: String, required: true },
+    gender: { type: String, enum: ["male", "female"] },
+    password: { type: String, required: true },
+    contactNo: { type: String, required: true },
+    emergencyContactNo: { type: String, required: true },
+    email: { type: String },
+    presentAddress: { type: String, required: true },
+    permanentAddress: { type: String, required: true },
+  });
+  // 3. Create a Model.
+  const User = model<IUser>("User", userSchema);
+  const createUserToDb = async () => {
+    const user = new User({
+      id: 777,
+      name: {
+        firstName: "happy jnnatul",
+        middleName: "koi",
+        lastName: "gelo",
+      },
 
+      role: "Student",
+      gender: "female",
+      password: " dhfjdhfjdhf",
+      contactNo: "0176544",
+      emergencyContactNo: "0882811",
+      email: "happy@gmail.com",
+      presentAddress: "kulgaon",
+      permanentAddress: "kulgao",
+    });
+    await user.save();
+
+    console.log(user.email);
+  };
+
+  createUserToDb();
   res.send("Hello World!");
   next();
 });
+
 export default app;
